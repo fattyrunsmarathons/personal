0254631ecbb3ef981d8c1214d8a56aed9ea8b66f
fattyrunsmarathons <45512462+fattyrunsmarathons@users.noreply.github.com>
2018-12-01 17:52:51 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+
+{
+int x;
+  printf ("enter the number \n");
+  scanf("%d",&x);
+  if (x>1)
+  printf ("hello world");
+  else
+  printf ("no hellos for you ");
+}
